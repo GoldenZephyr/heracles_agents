1465b011fa06144ece807631acddf4838a98bccf
arkinj <jarkin@mit.edu>
2025-09-13 12:53:26 -0400

    Add description of Mesh Place; Switch Region -> Room (#17)
    * Add description of Mesh Place; Switch Region -> Room
    
    * Add & Update pddl domain and scene graph description prompts
    
    -Add ordering enforcement to PDDL
    -Add description that predicates take scene graph symbols
    -Replace Region with Room
    -Add capitalization requirements to scene graph description for the two places layers

--- src/heracles_evaluation/pipelines/in_context_utils.py
+++ src/heracles_evaluation/pipelines/in_context_utils.py
@@ -15,40 +15,40 @@ class PromptingFailure(Exception):
 """ Methods for encoding a scene graph with ONLY Objects and Regions/Rooms """
 
 
-def get_region_parents_of_object(object_node, scene_graph):
-    """Method to return the string of the object node's grandparent region; this requires traversing the intermediate place layer"""
+def get_room_parents_of_object(object_node, scene_graph):
+    """Method to return the string of the object node's grandparent room; this requires traversing the intermediate place layer"""
     # Get the parents; returns a set of gtsam ids
     parent_place_gtsam_ids = object_node.parents()
     if not parent_place_gtsam_ids:
         return "none"
-    # Get the parent regions
-    parent_region_node_ids = set()
+    # Get the parent rooms
+    parent_room_node_ids = set()
     for parent_place_gtsam_id in parent_place_gtsam_ids:
         parent_place_node = scene_graph.get_node(parent_place_gtsam_id)
-        parent_region_gtsam_ids = parent_place_node.parents()
-        if not parent_region_gtsam_ids:
+        parent_room_gtsam_ids = parent_place_node.parents()
+        if not parent_room_gtsam_ids:
             continue
-        for parent_region_gtsam_id in parent_region_gtsam_ids:
-            parent_region_node = scene_graph.get_node(parent_region_gtsam_id)
-            parent_region_node_ids.add(parent_region_node.id.str(True))
-    if not parent_region_node_ids:
+        for parent_room_gtsam_id in parent_room_gtsam_ids:
+            parent_room_node = scene_graph.get_node(parent_room_gtsam_id)
+            parent_room_node_ids.add(parent_room_node.id.str(True))
+    if not parent_room_node_ids:
         return "none"
-    return parent_region_node_ids
+    return parent_room_node_ids
 
 
-def region_to_string(region_node, scene_graph):
-    attrs = region_node.attributes
-    symbol = region_node.id.str(True)
-    region_labelspace = scene_graph.get_labelspace(4, 0)
-    if not region_labelspace:
-        raise PromptingFailure("No available region labelspace")
-    semantic_type = region_labelspace.get_category(attrs.semantic_label)
-    region_string = f"\n-\t(id={symbol}, type={semantic_type})"
-    return region_string
+def room_to_string(room_node, scene_graph):
+    attrs = room_node.attributes
+    symbol = room_node.id.str(True)
+    room_labelspace = scene_graph.get_labelspace(4, 0)
+    if not room_labelspace:
+        raise PromptingFailure("No available room labelspace")
+    semantic_type = room_labelspace.get_category(attrs.semantic_label)
+    room_string = f"\n-\t(id={symbol}, type={semantic_type})"
+    return room_string
 
 
-def object_to_string_region_parent(object_node, scene_graph):
-    """Method to return the stirng of an object node; excludes the parent place and directly encodes the grandparent region"""
+def object_to_string_room_parent(object_node, scene_graph):
+    """Method to return the stirng of an object node; excludes the parent place and directly encodes the grandparent room"""
     attrs = object_node.attributes
     symbol = object_node.id.str(True)
     object_labelspace = scene_graph.get_labelspace(2, 0)
@@ -56,46 +56,48 @@ def object_to_string_region_parent(object_node, scene_graph):
         raise PromptingFailure("No available object labelspace")
     semantic_type = object_labelspace.get_category(attrs.semantic_label)
     position = f"({attrs.position[0]:.2f},{attrs.position[1]:.2f})"
-    parent_regions = get_region_parents_of_object(object_node, scene_graph)
-    object_string = f"\n-\t(id={symbol}, type={semantic_type}, pos={position}, parent_regions={parent_regions})"
+    parent_rooms = get_room_parents_of_object(object_node, scene_graph)
+    object_string = f"\n-\t(id={symbol}, type={semantic_type}, pos={position}, parent_rooms={parent_rooms})"
     return object_string
 
 
 def scene_graph_to_prompt(scene_graph):
-    """Method to produce a text encoding of a spark_dsg DynamicSceneGraph. This excludes 2D places and directly encodes a connection from objects to regions"""
+    """Method to produce a text encoding of a spark_dsg DynamicSceneGraph. This excludes 2D places and directly encodes a connection from objects to rooms"""
     # Add the objects
     objects_string = ""
     for object_node in scene_graph.get_layer(spark_dsg.DsgLayers.OBJECTS).nodes:
-        objects_string += object_to_string_region_parent(
+        objects_string += object_to_string_room_parent(
             object_node, scene_graph
         )  # Use the method that skips the place parent node
-    # Add the regions
-    regions_string = ""
-    for region_node in scene_graph.get_layer(spark_dsg.DsgLayers.ROOMS).nodes:
-        regions_string += region_to_string(region_node, scene_graph)
+    # Add the rooms
+    rooms_string = ""
+    for room_node in scene_graph.get_layer(spark_dsg.DsgLayers.ROOMS).nodes:
+        rooms_string += room_to_string(room_node, scene_graph)
     # Compose the strings into a single text encoding
-    scene_graph_string = f"<Scene Graph>\nObjects: {objects_string}\nRegions: {regions_string}</Scene Graph>"
+    scene_graph_string = (
+        f"<Scene Graph>\nObjects: {objects_string}\nRooms: {rooms_string}</Scene Graph>"
+    )
     return scene_graph_string
 
 
 """ Methods for encoding a scene graph with Objects, Places, and Regions/Rooms """
 
 
-def region_to_string_full(region_node, scene_graph):
-    """Method to compose a string encoding of a region: unique id, semantic type, and position"""
-    attrs = region_node.attributes
-    symbol = region_node.id.str(True)
-    region_labelspace = scene_graph.get_labelspace(4, 0)
-    if not region_labelspace:
-        raise PromptingFailure("No available region labelspace")
-    semantic_type = region_labelspace.get_category(attrs.semantic_label)
+def room_to_string_full(room_node, scene_graph):
+    """Method to compose a string encoding of a room: unique id, semantic type, and position"""
+    attrs = room_node.attributes
+    symbol = room_node.id.str(True)
+    room_labelspace = scene_graph.get_labelspace(4, 0)
+    if not room_labelspace:
+        raise PromptingFailure("No available room labelspace")
+    semantic_type = room_labelspace.get_category(attrs.semantic_label)
     position = f"({attrs.position[0]:.2f}, {attrs.position[1]:.2f})"
-    region_string = f"\n-\t(id={symbol}, type={semantic_type}, pos={position})"
-    return region_string
+    room_string = f"\n-\t(id={symbol}, type={semantic_type}, pos={position})"
+    return room_string
 
 
 def place_to_string_full(place_node, scene_graph):
-    """Method to compose a string encoding of a mesh place: unique id, sibling place unique ids, parent region/room unique ids"""
+    """Method to compose a string encoding of a mesh place: unique id, sibling place unique ids, parent room/room unique ids"""
     symbol = place_node.id.str(True)
     # Get the siblings
     sibling_node_ids = set()
@@ -106,15 +108,15 @@ def place_to_string_full(place_node, scene_graph):
     if not sibling_node_ids:
         sibling_node_ids = "none"
     # Get the parents
-    parent_region_node_ids = set()
-    parent_region_gtsam_ids = place_node.parents()
-    for parent_region_gtsam_id in parent_region_gtsam_ids:
-        parent_region_node = scene_graph.get_node(parent_region_gtsam_id)
-        parent_region_node_ids.add(parent_region_node.id.str(True))
-    if not parent_region_node_ids:
-        parent_region_node_ids = "none"
+    parent_room_node_ids = set()
+    parent_room_gtsam_ids = place_node.parents()
+    for parent_room_gtsam_id in parent_room_gtsam_ids:
+        parent_room_node = scene_graph.get_node(parent_room_gtsam_id)
+        parent_room_node_ids.add(parent_room_node.id.str(True))
+    if not parent_room_node_ids:
+        parent_room_node_ids = "none"
     # Compose the string
-    place_string = f"\n-\t(id={symbol}, siblings={sibling_node_ids}, parent_regions={parent_region_node_ids})"
+    place_string = f"\n-\t(id={symbol}, siblings={sibling_node_ids}, parent_rooms={parent_room_node_ids})"
     return place_string
 
 
@@ -145,13 +147,13 @@ def scene_graph_to_prompt_full(scene_graph, place_layer_name):
     objects_string = ""
     for object_node in scene_graph.get_layer(spark_dsg.DsgLayers.OBJECTS).nodes:
         objects_string += object_to_string_full(object_node, scene_graph)
-    # Add the places: unique id, sibling ids, parent region unique id
+    # Add the places: unique id, sibling ids, parent room unique id
     places_string = ""
     for place_node in scene_graph.get_layer(place_layer_name).nodes:
         places_string += place_to_string_full(place_node, scene_graph)
-    # Add the regions: unique id, semantic label, position
-    regions_string = ""
-    for region_node in scene_graph.get_layer(spark_dsg.DsgLayers.ROOMS).nodes:
-        regions_string += region_to_string_full(region_node, scene_graph)
-    scene_graph_string = f"<Scene Graph>\nObjects: {objects_string}\nPlaces: {places_string}\n Regions: {regions_string}\n</Scene Graph>"
+    # Add the rooms: unique id, semantic label, position
+    rooms_string = ""
+    for room_node in scene_graph.get_layer(spark_dsg.DsgLayers.ROOMS).nodes:
+        rooms_string += room_to_string_full(room_node, scene_graph)
+    scene_graph_string = f"<Scene Graph>\nObjects: {objects_string}\nPlaces: {places_string}\n Rooms: {rooms_string}\n</Scene Graph>"
     return scene_graph_string
