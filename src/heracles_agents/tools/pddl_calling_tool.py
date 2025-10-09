9988c0ee9fabff44c1136b722ccb012f7d303205
Aaron Ray <aaronray@mit.edu>
2025-10-09 18:23:00 +0000

    Enable sending of PDDL plans

--- src/heracles_agents/tools/pddl_calling_tool.py
+++ src/heracles_agents/tools/pddl_calling_tool.py
@@ -12,7 +12,7 @@ def send_pddl(pddl_goal_string, robot_name: str = None, planner_topic: str = Non
     ros2 topic pub {planner_topic} omniplanner_msgs/msg/PddlGoalMsg "{{robot_id: '{robot_name}', pddl_goal: '{pddl_goal_string}'}}" -1
     """
     print("cmd: ", cmd)
-    os.system(f"echo '{cmd}' > pddl_test.txt")
+    os.system(cmd)
     return f"Sent goal {pddl_goal_string} to robot {robot_name} on {planner_topic}"
 
 
